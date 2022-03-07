b0850b27918cfeb4790e0a8d81d4f5357fc73500
rehanhussain330 <80039576+rehanhussain330@users.noreply.github.com>
2022-03-07 03:44:16 -0800

    hi 3rd

---
+++ Js.js
@@ -0,0 +1,23 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
+ 
+
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+
+
+w3.slideshow(".nature", 1500);
+
+
